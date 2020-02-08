48e3d1a316aa3dde2bdce56537525c709bccf234
DezmonOnGit <Chernolichenko.ai@gmail.com>
2020-02-08 17:16:31 +0300

    add movies list and filter

---
+++ src/components/filter/filter.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Filter = (props) => {
+
+  const {filter, classes} = props;
+
+  return <ul className={classes.list}>
+    {
+      filter.map((item, index) => {
+
+        return <li key={index} className={index > 0 ? classes.item : classes.item + ` ` + classes.itemActive}>
+          <a href="#" className={classes.link}>{item}</a>
+        </li>;
+      })
+    }
+  </ul>;
+};
+
+Filter.propTypes = {
+  filter: PropTypes.array.isRequired,
+  classes: PropTypes.objectOf(
+      PropTypes.shape({
+        list: PropTypes.string.isRequired,
+        item: PropTypes.string.isRequired,
+        itemActive: PropTypes.string.isRequired,
+        link: PropTypes.string.isRequired,
+      })
+  )
+};
+
+export default Filter;
